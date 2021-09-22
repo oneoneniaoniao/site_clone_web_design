4e78846e95f4c4c55b13571687b2e0e91903d4ac
oneoneniaoniao <76186907+oneoneniaoniao@users.noreply.github.com>
2021-09-22 13:02:36 +0900

    create top page

--- js/main.js
+++ js/main.js
@@ -1,25 +1,13 @@
-// contact buttonの三角を動かす
-transparentBtn = document.getElementById("button_after")
-transparentBtn.addEventListener("click", () => {
-  transparentBtn.classList.add("clicked");
-  console.log(transparentBtn)
-  setTimeout(() => {
-    transparentBtn.classList.remove("clicked");
-  }, 400)
-});
-
 // sticky headerにするため
 const header = document.querySelector("header");
 window.addEventListener("scroll", () => {
   let value = window.scrollY
-  // console.log(value)
-  // if( window.matchMedia("(min-width:769px)").matches){
   if (value < 20) {
     header.classList.remove("is_fixed");
-    console.log("header is released", value)
-  } else if (value>30) {
+    // console.log("header is released", value)
+  } else if (value > 30) {
     header.classList.add("is_fixed");
-    console.log("header is fixed" , value)
+    // console.log("header is fixed", value)
   }
 })
 
@@ -27,7 +15,81 @@ window.addEventListener("scroll", () => {
 // ハンバーガーメニュー
 const btnMenu = document.getElementById("js-btnMenu");
 const gnavSP = document.getElementsByClassName("gnavSP")[0];
-btnMenu.addEventListener("click", ()=>{
+btnMenu.addEventListener("click", () => {
   btnMenu.classList.toggle("active");
- gnavSP.classList.toggle("show");
+  gnavSP.classList.toggle("show");
 })
+
+// スクロールトップボタン
+
+const scroll_to_top_btn = document.querySelector('#scroll_top_btn');
+scroll_to_top_btn.addEventListener('click', scroll_to_top);
+
+function scroll_to_top() {
+  window.scroll({
+    top: 0,
+    behavior: 'smooth'
+  });
+};
+window.addEventListener('scroll', scroll_event);
+function scroll_event() {
+  if (window.pageYOffset > 400) {
+    scroll_to_top_btn.style.opacity = '1';
+  } else if (window.pageYOffset < 400) {
+    scroll_to_top_btn.style.opacity = '0';
+  }
+};
+
+// スライドショー
+const imgs = [
+  './img/works/smoothiesta.jpg',
+  './img/works/web-conference.jpg',
+  './img/works/lamina.jpg'
+]
+// imgs.forEach((img, index) => {
+//   let imgTag= document.createElement("img");
+//   imgTag.src = img
+// })
+
+const target = document.getElementById("slide_img");
+const dots = document.querySelectorAll(".dots>.dot");
+const MS = 2500;
+
+let current = 0;
+dots[current].classList.add("selected");
+
+const select_img = current => {
+  target.style.backgroundImage = `url(${imgs[current]})`;
+  dots.forEach(dot => {
+    dot.classList.remove("selected");
+  })
+  dots[current].classList.add("selected");
+}
+
+const changeImg = num => {
+  if (current + num >= 0 && current + num < imgs.length) {
+    current += num;
+  } else if (current + num == -1) {
+    current = imgs.length - 1;
+  } else if (current + num == imgs.length) {
+    current = 0;
+  }
+  select_img(current)
+}
+
+dots.forEach((dot, index) => {
+  dot.onclick = () => {
+    select_img(index)
+    clearTimeout(timer);
+  }
+})
+
+const timer = setInterval(changeImg, MS, 1);
+document.getElementById("prev").onclick = () => {
+  changeImg(-1);
+  clearTimeout(timer);
+}
+document.getElementById("next").onclick = () => {
+  changeImg(1);
+  clearTimeout(timer);
+}
